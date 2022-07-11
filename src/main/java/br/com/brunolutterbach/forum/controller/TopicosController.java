39e4371c3aef60af30706b96255877d73d001aca
Bruno <brunolutterbach13@gmail.com>
2022-07-11 18:53:07 -0300

    Injeção de TopicoRepository

--- src/main/java/br/com/brunolutterbach/forum/controller/TopicosController.java
+++ src/main/java/br/com/brunolutterbach/forum/controller/TopicosController.java
@@ -1,8 +1,7 @@
 package br.com.brunolutterbach.forum.controller;
 
 import br.com.brunolutterbach.forum.controller.dto.TopicoDto;
-import br.com.brunolutterbach.forum.modelo.Curso;
-import br.com.brunolutterbach.forum.modelo.Topico;
+import br.com.brunolutterbach.forum.repository.TopicoRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +11,14 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
+    public final TopicoRepository topicoRepository;
+
+    public TopicosController(TopicoRepository topicoRepository) {
+        this.topicoRepository = topicoRepository;
+    }
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista() {
-        Topico topico = new Topico("Duvida", "Como funciona o Spring MVC?", new Curso("Spring", "Programação"));
-
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        return TopicoDto.converter(topicoRepository.findAll());
     }
 }
