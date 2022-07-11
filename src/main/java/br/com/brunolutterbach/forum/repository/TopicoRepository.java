2818255f69459b954da12b5179f5b50448b776a6
Bruno <brunolutterbach13@gmail.com>
2022-07-11 19:42:42 -0300

    .

--- src/main/java/br/com/brunolutterbach/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/brunolutterbach/forum/repository/TopicoRepository.java
@@ -3,6 +3,7 @@ package br.com.brunolutterbach.forum.repository;
 import br.com.brunolutterbach.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
     @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
-    List<Topico> listarPorNomeDoCurso(String nomeCurso);
+    List<Topico> listarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 
 }
 
