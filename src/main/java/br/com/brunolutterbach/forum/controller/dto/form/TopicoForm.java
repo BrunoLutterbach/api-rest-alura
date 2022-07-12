42746a882e741e2ce25d1fa31a8a1b51eb62cc3f
Bruno <brunolutterbach13@gmail.com>
2022-07-11 22:05:45 -0300

    Validação do método cadastrar

--- src/main/java/br/com/brunolutterbach/forum/controller/dto/form/TopicoForm.java
+++ src/main/java/br/com/brunolutterbach/forum/controller/dto/form/TopicoForm.java
@@ -4,12 +4,19 @@ import br.com.brunolutterbach.forum.modelo.Curso;
 import br.com.brunolutterbach.forum.modelo.Topico;
 import br.com.brunolutterbach.forum.repository.CursoRepository;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class TopicoForm {
 
+    @NotNull @NotEmpty @Length(min = 5)
     private String titulo;
+    @NotNull @NotEmpty @Length(min = 10)
     private String mensagem;
+    @NotNull @NotEmpty
     private String nomeCurso;
 
     public Topico converter(CursoRepository repository) {
