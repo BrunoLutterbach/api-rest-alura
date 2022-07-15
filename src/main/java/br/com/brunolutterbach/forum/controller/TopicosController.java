383bf64aacd0295bfb432faa42c5f2c36414afb5
Bruno <brunolutterbach13@gmail.com>
2022-07-15 20:09:36 -0300

    Adicionado Pageable como parâmetro do método lista

--- src/main/java/br/com/brunolutterbach/forum/controller/TopicosController.java
+++ src/main/java/br/com/brunolutterbach/forum/controller/TopicosController.java
@@ -34,9 +34,7 @@ public class TopicosController {
     }
 
     @GetMapping()
-    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
-
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, Pageable paginacao) {
 
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
